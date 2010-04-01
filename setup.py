4a4864337d38e311750d7a28b6312446ee610ba3
Ivan Gromov <summer.is.gone@gmail.com>
2010-04-02 01:58:25 +0600

    A final upload to pypi. it working! :)

--- setup.py
+++ setup.py
@@ -1,7 +1,29 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="gitrecipe",
-    entry_points={'zc.buildout': ['default = recipe:GitRecipe']},
-    )
+    version='0.0.1',
+    description='Simple buildout recipe for downloading git repositories. It uses system git command and its syntax',
+    author='Ivan Gromov',
+    author_email='summer.is.gone@gmail.com',
+    url='http://github.com/summerisgone/gitrecipe',
+    download_url='http://github.com/summerisgone/gitrecipe/zipball/0.1',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Buildout',
+        'Intended Audience :: Developers',
+        'License :: Freely Distributable',
+        'Natural Language :: Russian',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2.5',
+        'Topic :: Software Development :: Version Control',
+    ],
+    namespace_packages=['recipe'],
+    packages=find_packages(),
+    install_requires=['setuptools', 'zc.recipe.egg'],
+    entry_points={'zc.buildout': ['default = recipe.git:GitRecipe']},
+    zip_safe=False,
+    long_description=open('README.rst').read(),
+)
